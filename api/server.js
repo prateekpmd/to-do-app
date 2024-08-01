4199a3fff9e90eacecf5c9450d55f137f875033b
prateekpmd <premanandprateek@gmail.com>
2024-08-01 10:12:15 +0530

    cors

--- api/server.js
+++ api/server.js
@@ -9,7 +9,12 @@ const Todo = require('./models/Todo');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+	origin: 'https://to-do-app-theta-flax.vercel.app', // Replace with your actual frontend URL
+	optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+  };
+  
+  app.use(cors(corsOptions));
 
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true, 
